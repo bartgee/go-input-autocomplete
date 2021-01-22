c5ffb4398c40e3da71326b9c627d752987c2a7c3
Bart Grzybicki <bgrzybicki@gmail.com>
2021-01-22 14:50:43 +0100

    add some test prints

--- enable_virtual_terminal.go
+++ enable_virtual_terminal.go
@@ -3,6 +3,7 @@
 package input_autocomplete
 
 import (
+	"fmt"
 	"os"
 
 	"golang.org/x/sys/windows"
@@ -10,11 +11,15 @@ import (
 
 func EnableVirtalTerminalWindows() error {
 	var originalMode uint32
+	fmt.Println("before windows.Handle")
 	stdout := windows.Handle(os.Stdout.Fd())
 
+	fmt.Println("before windows.GetConsoleMode")
 	if err := windows.GetConsoleMode(stdout, &originalMode); err != nil {
+		fmt.Println("windows.GetConsoleMode error")
 		return err
 	}
 
+	fmt.Println("before windows.SetConsoleMode")
 	return windows.SetConsoleMode(stdout, originalMode|windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 }
