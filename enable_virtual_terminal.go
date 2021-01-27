a662f0dd6cc600de9d3904a3aaf37c8eee738373
Bart Grzybicki <bgrzybicki@gmail.com>
2021-01-27 16:59:12 +0100

    virtual terminal input instead of processing

--- enable_virtual_terminal.go
+++ enable_virtual_terminal.go
@@ -61,7 +61,7 @@ func EnableVirtualTerminalWindows() error {
 	stdout := windows.Handle(os.Stdout.Fd())
 
 	windows.GetConsoleMode(stdout, &originalMode)
-	windows.SetConsoleMode(stdout, originalMode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	// windows.SetConsoleMode(stdout, originalMode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 	windows.SetConsoleMode(stdout, originalMode | windows.ENABLE_VIRTUAL_TERMINAL_INPUT)
 	defer windows.SetConsoleMode(stdout, originalMode)
 	return nil
