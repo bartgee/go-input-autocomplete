7696b504ebca160d4e83fb4dc99eab4a58643e69
Bart Grzybicki <bgrzybicki@gmail.com>
2021-01-27 16:31:11 +0100

    test some other code

--- enable_virtual_terminal.go
+++ enable_virtual_terminal.go
@@ -3,6 +3,8 @@
 package input_autocomplete
 
 import (
+	"golang.org/x/sys/windows"
+	"os"
 	"syscall"
 )
 
@@ -31,25 +33,35 @@ var (
 // 	return windows.SetConsoleMode(stdout, originalMode)
 // }
 
+// func EnableVirtualTerminalWindows() error {
+// 	const ENABLE_VIRTUAL_TERMINAL_PROCESSING uint32 = 0x4
+//
+// 	var mode uint32
+// 	err := syscall.GetConsoleMode(syscall.Stdout, &mode)
+// 	if err != nil {
+// 		return err
+// 	}
+//
+// 	// if enable {
+// 	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING
+// 	// } else {
+// 	// 	mode &^= ENABLE_VIRTUAL_TERMINAL_PROCESSING
+// 	// }
+//
+// 	ret, _, err := setConsoleMode.Call(uintptr(syscall.Stdout), uintptr(mode))
+// 	if ret == 0 {
+// 		return err
+// 	}
+//
+// 	return nil
+// }
+
 func EnableVirtualTerminalWindows() error {
-	const ENABLE_VIRTUAL_TERMINAL_PROCESSING uint32 = 0x4
+	var originalMode uint32
+	stdout := windows.Handle(os.Stdout.Fd())
 
-	var mode uint32
-	err := syscall.GetConsoleMode(syscall.Stdout, &mode)
-	if err != nil {
-		return err
-	}
-
-	// if enable {
-	mode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING
-	// } else {
-	// 	mode &^= ENABLE_VIRTUAL_TERMINAL_PROCESSING
-	// }
-
-	ret, _, err := setConsoleMode.Call(uintptr(syscall.Stdout), uintptr(mode))
-	if ret == 0 {
-		return err
-	}
-
+	windows.GetConsoleMode(stdout, &originalMode)
+	windows.SetConsoleMode(stdout, originalMode | windows.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+	defer windows.SetConsoleMode(stdout, originalMode)
 	return nil
 }
